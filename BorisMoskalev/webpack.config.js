2e492cb12a173cab9f9a6864871c2aef256b0158
bmoskalev <mbs20111980@bk.ru>
2019-11-23 21:20:07 +0500

    Add final changes for homework1

--- BorisMoskalev/webpack.config.js
+++ BorisMoskalev/webpack.config.js
@@ -8,6 +8,15 @@ module.exports = {
         path: path.resolve(__dirname,'dist'),
         filename: 'bundle.js',
     },
+    module: {
+        rules: [
+            {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            }
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'src', 'index.html'),
