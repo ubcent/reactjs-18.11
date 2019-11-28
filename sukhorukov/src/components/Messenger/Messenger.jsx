01b2e6dd7eb9852ba985752bc30dfe1525556eeb
Sergeygeek <sukhorukov1986@yandex.ru>
2019-11-28 05:28:54 +0400

    Lesson 3

--- sukhorukov/src/components/Messenger/Messenger.jsx
+++ sukhorukov/src/components/Messenger/Messenger.jsx
@@ -1,7 +1,9 @@
+import './Messenger.css';
+
 import React, { Component } from 'react';
 
-import { MessagesList } from './MessagesList';
-import { MessageForm } from './MessageForm';
+import { MessagesList } from 'components/MessagesList';
+import { MessageForm } from 'components/MessageForm';
 
 const messages = ['Привет!', 'Как дела?', 'Как настроение?', 'Как погода?'];
 
@@ -16,26 +18,24 @@ export class Messenger extends Component {
   }
 
   componentDidUpdate() {
-    if(this.state.messages[this.state.messages.length -1].author !== "Bot"){
-      setTimeout(() => {
-        let human = this.state.messages[this.state.messages.length -1].author 
+    const lastMessage = this.state.messages[this.state.messages.length -1];
+    if(lastMessage.author !== "Bot"){
+      setTimeout(() => { 
         this.setState({
-          messages: this.state.messages.concat([{author: "Bot", text: `Привет ${human}, я бот. Я пока еще тупой!`}])
+          messages: this.state.messages.concat([{author: "Bot", text: `Привет ${lastMessage.author}, я бот. Я пока еще тупой!`}])
         })
       }, 1000);
     }
   }
 
   handleMessageSend = (message) => {
-    this.setState({
-      messages: this.state.messages.concat([{author: message.author, text: message.text}])
-    })
+    this.setState(({messages}) => ({ messages: messages.concat([message]) }));
   }
 
   render() {
     const { messages } = this.state;
     return(
-      <div>
+      <div className="messenger">
         <MessagesList messages={messages}/>
         <MessageForm onSend={this.handleMessageSend} />
       </div>
