ea51a9e0e53ad416560c1a23f2bea611903e5849
NichoMan26 <basenkobob@mail.ru>
2019-12-02 15:49:18 +0200

    lesson 4

--- basenko_denis/src/components/UserList/UserList.jsx
+++ basenko_denis/src/components/UserList/UserList.jsx
@@ -1,19 +1,19 @@
 import './userList.css'
 
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export default class extends React.Component{
     render(){
         return(
             <div className='userList'>
                 <ul className='userList__ul'>
-                    <li className='userList__li userList__li_active'>OLEG</li>
-                    <li className='userList__li'>IVAN</li>
-                    <li className='userList__li'>VOVA</li>
-                    <li className='userList__li'>GLEB</li>
-                    <li className='userList__li'>VASYA</li>
+                    <li className='userList__li userList__li_active'><Link className='userList__a' to='/chat/1'>OLEG</Link></li>
+                    <li className='userList__li'><Link className='userList__a' to='/chat/2'>IVAN</Link></li>
+                    <li className='userList__li'><Link className='userList__a' to='/chat/3'>VOVA</Link></li>
+                    <li className='userList__li'><Link className='userList__a' to='/chat/4'>GLEB</Link></li>
+                    <li className='userList__li'><Link className='userList__a' to='/chat/5'>VASYA</Link></li>
                 </ul>
             </div>)
-        
     }
 }
