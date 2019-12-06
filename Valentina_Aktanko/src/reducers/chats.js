07b93f3b244b27058473fc54080cff4734f03fa0
Valentina-Aktanko <freyakhv@gmail.com>
2019-12-06 15:14:12 +0300

    syntax with redux-action

--- Valentina_Aktanko/src/reducers/chats.js
+++ Valentina_Aktanko/src/reducers/chats.js
@@ -1,8 +1,7 @@
 import { handleActions } from 'redux-actions';
 
-export const chatsReducer = function (action, state) {
-    switch(action.type) {
-        case SEND_MESSAGE:
-            
-    }
-}
+const initialState = {}
+
+export const chatsReducer = handleSctions({
+
+}, initialState);
