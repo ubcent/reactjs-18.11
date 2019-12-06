01b4f25fe3446211aed42b1d7786660f4038aae2
Valentina-Aktanko <freyakhv@gmail.com>
2019-12-06 11:54:35 +0300

    Provider Redux

--- Valentina_Aktanko/src/index.jsx
+++ Valentina_Aktanko/src/index.jsx
@@ -3,15 +3,18 @@ import 'assets/global.css';
 import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import { routes } from './routes';
 
 ReactDom.render (
-    <BrowserRouter>
-        <Switch>
-            {routes.map((route, idx) => <Route key={idx} {...route} />)}
-        </Switch>
-    </BrowserRouter>,
+    <Provider>
+        <BrowserRouter>
+            <Switch>
+                {routes.map((route, idx) => <Route key={idx} {...route} />)}
+            </Switch>
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root'), 
     
 );
