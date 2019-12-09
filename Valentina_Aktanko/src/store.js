8640765e36be1005bb2d2dd6d4b56ad4686f66fc
Valentina-Aktanko <freyakhv@gmail.com>
2019-12-09 13:35:49 +0300

    own logger (Redux Middleware)

--- Valentina_Aktanko/src/store.js
+++ Valentina_Aktanko/src/store.js
@@ -1,5 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
+
+function logger(store) {
+    return function wrapDispatch(next) {
+        return function dispatchAndLog(action) {
+            console.log('Action' , action);
+            console.log('prevState', store.getState().chats.toJS());
+
+            const result = next(action);
+            console.log('nextState', store.getState().chats.toJS());
+
+            return result;
+        }
+    }
+}
+// logger()()(); 
+
 
 import { rootReducer } from 'reducers';
 
